af68e23dcdddf7ac7c7d2385a6a9dd380c982b75
Daniel Budziński <artuditu74@gmail.com>
2017-03-19 22:21:44 +0100

    Update kukusnackbar.js

--- kukusnackbar.js
+++ kukusnackbar.js
@@ -1,11 +1,9 @@
 /*
-##############################################
-#
-#
-#
-#
-#
-##############################################
+Project: kukuSnackBar
+Author: Daniel 'Danieo' Budziński
+Version: 01.00.00
+License: Apache 2.0
+Github: https://github.com/Danieo74/kukuSnackBar
 */
 function kukuSnackbar(defaultStyle)
 {
